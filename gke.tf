d6f375fb3cd9b4d33ec57d774b3b46a24957ff95
Manish Warang <manish6385@gmail.com>
2020-12-12 00:35:25 +0530

    added relevant scope

--- gke.tf
+++ gke.tf
@@ -10,7 +10,7 @@ resource "google_container_cluster" "kubernetes" {
   initial_node_count       = var.gke_num_nodes
   remove_default_node_pool = var.remove_default_node_pool
 
-  network = google_compute_network.gke_vpc.name
+  network    = google_compute_network.gke_vpc.name
   subnetwork = google_compute_subnetwork.gke_subnet.name
 
   # network    = google_compute_network.vpc.name
@@ -23,6 +23,17 @@ resource "google_container_cluster" "kubernetes" {
     client_certificate_config {
       issue_client_certificate = false
     }
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    tags = ["network-cluster"]
   }
 
   depends_on = [google_project_service.kubernetes]
